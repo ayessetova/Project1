4c9433738bf0f6e3ebc30186475015ab0200c30b
Anar Yessetova <naraailana14@gmail.com>
2022-03-04 18:28:53 -0600

    add package.json and type module into the file

--- helpers.js
+++ helpers.js
@@ -1,23 +1,8 @@
-// function doubleNumber(n){
-//     return n*2;
-// }
-//
-// function initial(str){
-//     // let arr = [];
-//     let arr = str.split(' ');
-//     console.log(arr2)
-//     return `${arr[0]} ${arr[1][0]}. ${arr[2][0]}.`
-// }
-//
-// module.exports =  doubleNumber
-// module.exports = initial
-export function doubleNumber(n){
-    return n*2;
+export function doubleNumber(n) {
+    return n * 2;
 }
 
-export function initial(str){
-    // let arr = [];
+export function initial(str) {
     let arr = str.split(' ');
-    console.log(arr)
-    return `${arr[0]} ${arr[1][0]}. ${arr[2][0]}.`
+    return `${arr[0]} ${arr[1][0]}. ${arr[2][0]}.`;
 }
--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// const doubleNumber = require('./helpers')
-// const initial = require('./helpers')
-//
-// // console.log(initial('Alice smith adams'));
-// console.log(doubleNumber(6));
+import { doubleNumber, initial } from "./helpers.js";
 
-import { doubleNumber, initial } from "./helpers";
-
-console.log(initial('Alice smith adams'));
-// console.log(doubleNumber(6));
+console.log(initial('Alice Smith Adams'));
+console.log(doubleNumber(6));
